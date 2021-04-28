aefac6a30adb01dade208795d04cce78e1640f8b
serifegnll <gonulluserife7@gmail.com>
2021-04-28 12:46:39 +0300

    added a code example that finds file names for read&write functions

--- src/main.c
+++ src/main.c
@@ -1,10 +1,11 @@
 #include <stdio.h>
+#include <string.h>
 #include "../include/fields.h"
-void readInputFile(){
+void readInputFile(char*filename){
     IS is;
     int i;
 
-    is = new_inputstruct("a.txt");
+    is = new_inputstruct(filename);
     if (is == NULL) {
         perror("error");
         exit(1);
@@ -18,15 +19,45 @@ void readInputFile(){
     jettison_inputstruct(is);
 }
 
-void writeOutputFile(char *data){
-    FILE *fp = fopen("output.txt","a");
+void writeOutputFile(char *data,char*filename){
+    FILE *fp = fopen(filename,"a");
     fprintf(fp,"%s \n", data);
 	fclose(fp);
 }
 
 int main(int argc, char** argv) {
     //readInputFile();
-    writeOutputFile("hello");
-    writeOutputFile("world");
+    
+	
+	
+	int d=0;
+	char* parameter;
+	char *inputFileName;
+	char *outputFileName;
+	const char *delp;
+	char* dz[4];
+	printf("komut girin\n");
+	char command[40];
+	scanf("%[^\n]s",command);
+	delp = strtok (command, " &");
+	while (delp != NULL)  {
+      printf ("%s\n", delp);
+	  dz[d]=delp;	  
+	  d++;	  
+      delp = strtok (NULL, " &");
+    }
+	
+	parameter=dz[1];
+	inputFileName=dz[2];
+	outputFileName=dz[3];
+	
+	//printf (parameter);
+	//printf (inputFileName);
+	//printf (outputFileName);
+	
+	writeOutputFile("hello",outputFileName);
+    
+	
+	
     return 0;
 }
